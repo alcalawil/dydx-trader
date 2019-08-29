9f9f1c60f14adbc0a9566c38c4b90836523c6bca
Wilfredo Alcala <alcalawilfre@gmail.com>
2019-08-29 17:36:54 -0300

    Add funds route

---
+++ src/routes/Funds.ts
@@ -0,0 +1,38 @@
+import { logger } from '@shared';
+import { Request, Response, Router } from 'express';
+import { BAD_REQUEST, CREATED, OK } from 'http-status-codes';
+
+const router = Router();
+
+/******************************************************************************
+ *                      Get active orders - "GET /api/funds/balance"
+ ******************************************************************************/
+
+router.get('/balance', async (req: Request, res: Response) => {
+  try {
+    return res.status(OK).json({});
+  } catch (err) {
+    logger.error(err.message, JSON.stringify(err));
+    return res.status(BAD_REQUEST).json({
+      error: err.message
+    });
+  }
+});
+
+/******************************************************************************
+ *                       Deposit Funds - "POST /api/funds/balance"
+ ******************************************************************************/
+
+router.post('/deposit', async (req: Request, res: Response) => {
+  try {
+    // TODO: Return receipt
+    return res.status(CREATED).end();
+  } catch (err) {
+    logger.error(err.message, err);
+    return res.status(BAD_REQUEST).json({
+      error: err.message
+    });
+  }
+});
+
+export default router;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import OrdersRouter from './Orders';
+import FundsRouter from './Funds';
 
 const router = Router();
 
 // Add sub-routes
 router.use('/orders', OrdersRouter);
+router.use('/funds', FundsRouter);
 
 export default router;
