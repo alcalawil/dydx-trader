cc84ccfc3e5fbc3fc123ae9ced5210c55aa3f0a2
carlosjgomez <carlos.gomez311096@gmail.com>
2019-09-10 14:52:31 -0400

    added awsManager module

--- src/modules/awsManager.ts
+++ src/modules/awsManager.ts
@@ -1,71 +1,72 @@
 import AWS from 'aws-sdk';
 
 AWS.config.update({
-    accessKeyId: process.env.ACCESS_KEY_ID,
-    secretAccessKey: process.env.SECRET_ACCESS_KEY
+  accessKeyId: process.env.ACCESS_KEY_ID,
+  secretAccessKey: process.env.SECRET_ACCESS_KEY
 });
 
 const kms = new AWS.KMS({
-    region: process.env.KMS_REGION
+  region: process.env.KMS_REGION
 });
 
 const sns = new AWS.SNS({
-    region: process.env.SNS_REGION
+  region: process.env.SNS_REGION
 });
 
-class awsManager {
+class AwsManager {
+  public decrypt(encryptedData: string) {
+    return new Promise((resolve, reject) => {
+      const params = {
+        CiphertextBlob: Buffer.from(encryptedData, 'base64')
+      };
 
-    public decrypt(data: string) {
-        return new Promise((resolve, reject) => {
-            const params = {
-                CiphertextBlob: Buffer.from(data, 'base64')
-            };
-            kms.decrypt(params, (err, data) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    const result: any = data.Plaintext;
-                    resolve(Buffer.from(result).toString());
-                }
-            });
-        });
-    }
+      kms.decrypt(params, (err, data) => {
+        if (err) {
+          reject(err);
+        } else {
+          const result: any = data.Plaintext;
+          resolve(Buffer.from(result).toString());
+        }
+      });
+    });
+  }
 
-    public encrypt(data: string) {
-        return new Promise((resolve: any, reject: any) => {
-            const key: any = process.env.KEY_ID;
-            const params = {
-                KeyId: key, // The identifier of the CMK to use for encryption. You can use the key ID or Amazon Resource Name (ARN) of the CMK, or the name or ARN of an alias that refers to the CMK.
-                Plaintext: data
-            };
-            kms.encrypt(params, (err, data) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    const result: any = data.CiphertextBlob;
-                    resolve(Buffer.from(result).toString('base64'));
-                }
-            });
-        });
-    }
+  public encrypt(plainText: string) {
+    return new Promise((resolve: any, reject: any) => {
+      const key: any = process.env.KEY_ID;
+      const params = {
+        Plaintext: plainText,
+        KeyId: key // The identifier of the CMK to use for encryption.
+        // You can use the key ID or Amazon Resource Name (ARN) of the CMK,
+        // or the name or ARN of an alias that refers to the CMK.
+      };
 
-    public publish(msg: any) {
-        return new Promise(async (resolve, reject) => {
-            var publishParams: AWS.SNS.PublishInput = {
-                TopicArn: process.env.SNS_ARN,
-                Message: JSON.stringify(msg.Message),
-                MessageAttributes: msg.MessageAttributes
-            };
+      kms.encrypt(params, (err, data) => {
+        if (err) {
+          return reject(err);
+        }
+        const result: any = data.CiphertextBlob;
+        resolve(Buffer.from(result).toString('base64'));
+      });
+    });
+  }
 
-            sns.publish(publishParams, function (err, data) {
-                if (err) {
-                    reject(err);
-                }
-                resolve(data);
-            });
-        });
-    }
+  public publish(msg: any) {
+    return new Promise(async (resolve, reject) => {
+      const publishParams: AWS.SNS.PublishInput = {
+        TopicArn: process.env.SNS_ARN,
+        Message: JSON.stringify(msg.Message),
+        MessageAttributes: msg.MessageAttributes
+      };
+
+      sns.publish(publishParams, (err, data) => {
+        if (err) {
+          return reject(err);
+        }
+        resolve(data);
+      });
+    });
+  }
 }
 
-
-export default () => new awsManager();
+export default () => new AwsManager();
