da240096bc46d67760e8e0b4c9812a6ac5380a99
carlosjgomez <carlos.gomez311096@gmail.com>
2019-11-05 13:53:49 -0400

    fixed some bugs and added script into package.json

--- scripts/spreadOrders/index.js
+++ scripts/spreadOrders/index.js
@@ -1,12 +1,12 @@
 const SpreadOrders = require('./SpreadOrders');
-const { 
-  doGetRequest, 
-  PriceDetail, 
-  cancelOrder, 
+const {
+  doGetRequest,
+  PriceDetail,
+  cancelOrder,
   postOrder
 } = require('./helpers');
-const { 
-  _range, 
+const {
+  _range,
   GET_ASK_URI,
   GET_BID_URI,
   HITBTC_ETHDAI_TICKER,
@@ -24,20 +24,20 @@ const tradingCycle = async () => {
   const hitbtcPrice = await doGetRequest({ uri: HITBTC_ETHDAI_TICKER });
 
   const internalPrice = new PriceDetail(dydxAsk, dydxBid);
-  const externalPrice = new PriceDetail(hitbtcPrice.ask, hitbtcPrice.bid);
-  
+  const externalPrice = new PriceDetail(parseFloat(hitbtcPrice.ask), parseFloat(hitbtcPrice.bid));
+
   // Cancel all
-  const canceledOrders = await cancelOrders(_openOrders);
-  
+  const canceledOrders = await cancelOrders(_myOrders);
+
   // Remove all canceled orders
   // TODO: remover solo si el cancel fue success o si la orden fue cancelada antes
-  canceledOrders.map(canceledOrder => 
+  canceledOrders.map(canceledOrder =>
     removeOrderFromRegistry(canceledOrder.id)
   );
-  
+
   // Generate new orders from rules
-  const cexOrders = spreadOrders.output({ internalPrice, externalPrice });
-  
+  const cexOrders = spreadOrders.outputOrders({ internalPrice, externalPrice });
+
   // Post generated orders
   const responseOrders = await postMany(cexOrders);
   // Save successfully posted orders in registry
@@ -47,19 +47,19 @@ const tradingCycle = async () => {
 // =============================================================================================
 
 // TODO: Handlear errores para que retorne solamente ordenes correctamente posteadas
-const postMany =  (cexOrders) => Promise.all(
+const postMany = (cexOrders) => Promise.all(
   cexOrders.map(order => postOrder(order))
 );
 
 // TODO: Handlear errores para que retorne solo responses de ordenes correctamente canceladas
 const cancelOrders = (orders) => Promise.all(
   orders.map((order) => cancelOrder(order.id))
-);      
-      
+);
+
 const removeOrderFromRegistry = (orderId) => {
   _myOrders = _myOrders.filter(myOrder => myOrder.id !== orderId);
 };
-      
+
 
 const startCycle = () => {
   tradingCycle();
