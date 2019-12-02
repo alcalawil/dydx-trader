24d63782d7f0e2f821387e9454b179781690e556
Carlos Gomez <carlos.gomez311096@gmail.com>
2019-12-02 14:41:10 -0400

    update solo version

--- src/constants/Tokens.ts
+++ src/constants/Tokens.ts
@@ -2,22 +2,28 @@ import { IToken } from '@entities';
 import { MarketId } from '@dydxprotocol/solo';
 
 export const DYDX_TOKENS: IToken[] = [
-    {
-      id: MarketId.WETH.toNumber(),
-      shortName: 'WETH',
-      weiUnit: 'e18',
-      isBase: false
-    },
-    {
-      id: MarketId.DAI.toNumber(),
-      shortName: 'DAI',
-      weiUnit: 'e18',
-      isBase: true
-    },
-    {
-      id: MarketId.USDC.toNumber(),
-      shortName: 'USDC',
-      weiUnit: 'e6',
-      isBase: true
-    }
+  {
+    id: MarketId.WETH.toNumber(),
+    shortName: 'WETH',
+    weiUnit: 'e18',
+    isBase: false
+  },
+  {
+    id: MarketId.DAI.toNumber(),
+    shortName: 'DAI',
+    weiUnit: 'e18',
+    isBase: true
+  },
+  {
+    id: MarketId.USDC.toNumber(),
+    shortName: 'USDC',
+    weiUnit: 'e6',
+    isBase: true
+  },
+  {
+    id: MarketId.SAI.toNumber(),
+    shortName: 'SAI',
+    weiUnit: 'e18',
+    isBase: true
+  }
 ];
